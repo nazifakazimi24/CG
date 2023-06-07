5939d5bd814ad791af50b5dfde2c37629afca05f
nazkazimi24 <NazKazimi24@gmail.com>
2023-06-07 19:39:39 +0200

    new

---
+++ build/external/glbinding-2.1.1/source/glbinding/CMakeFiles/glbinding.dir/source/CallbackMask.cpp.o.d
@@ -0,0 +1,16 @@
+external/glbinding-2.1.1/source/glbinding/CMakeFiles/glbinding.dir/source/CallbackMask.cpp.o: \
+ /home/diro7061/CGLab_JasonBrockmeyer11686_NazifaKazimi1236886/external/glbinding-2.1.1/source/glbinding/source/CallbackMask.cpp \
+ /usr/include/stdc-predef.h \
+ /home/diro7061/CGLab_JasonBrockmeyer11686_NazifaKazimi1236886/external/glbinding-2.1.1/source/glbinding/include/glbinding/CallbackMask.h \
+ /home/diro7061/CGLab_JasonBrockmeyer11686_NazifaKazimi1236886/build/external/glbinding-2.1.1/source/glbinding/include/glbinding/glbinding_api.h \
+ /usr/include/c++/11/type_traits \
+ /usr/include/x86_64-linux-gnu/c++/11/bits/c++config.h \
+ /usr/include/x86_64-linux-gnu/c++/11/bits/os_defines.h \
+ /usr/include/features.h /usr/include/features-time64.h \
+ /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+ /usr/include/x86_64-linux-gnu/bits/timesize.h \
+ /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+ /usr/include/x86_64-linux-gnu/bits/long-double.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+ /usr/include/x86_64-linux-gnu/c++/11/bits/cpu_defines.h
---
+++ build/external/glbinding-2.1.1/source/glbinding/CMakeFiles/glbinding.dir/source/ProcAddress.cpp.o.d
@@ -0,0 +1,48 @@
+external/glbinding-2.1.1/source/glbinding/CMakeFiles/glbinding.dir/source/ProcAddress.cpp.o: \
+ /home/diro7061/CGLab_JasonBrockmeyer11686_NazifaKazimi1236886/external/glbinding-2.1.1/source/glbinding/source/ProcAddress.cpp \
+ /usr/include/stdc-predef.h \
+ /home/diro7061/CGLab_JasonBrockmeyer11686_NazifaKazimi1236886/external/glbinding-2.1.1/source/glbinding/include/glbinding/ProcAddress.h \
+ /home/diro7061/CGLab_JasonBrockmeyer11686_NazifaKazimi1236886/build/external/glbinding-2.1.1/source/glbinding/include/glbinding/glbinding_api.h \
+ /usr/include/GL/glx.h /usr/include/X11/Xlib.h \
+ /usr/include/x86_64-linux-gnu/sys/types.h /usr/include/features.h \
+ /usr/include/features-time64.h \
+ /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+ /usr/include/x86_64-linux-gnu/bits/timesize.h \
+ /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+ /usr/include/x86_64-linux-gnu/bits/long-double.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+ /usr/include/x86_64-linux-gnu/bits/types.h \
+ /usr/include/x86_64-linux-gnu/bits/typesizes.h \
+ /usr/include/x86_64-linux-gnu/bits/time64.h \
+ /usr/include/x86_64-linux-gnu/bits/types/clock_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/clockid_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/time_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/timer_t.h \
+ /usr/lib/gcc/x86_64-linux-gnu/11/include/stddef.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-intn.h /usr/include/endian.h \
+ /usr/include/x86_64-linux-gnu/bits/endian.h \
+ /usr/include/x86_64-linux-gnu/bits/endianness.h \
+ /usr/include/x86_64-linux-gnu/bits/byteswap.h \
+ /usr/include/x86_64-linux-gnu/bits/uintn-identity.h \
+ /usr/include/x86_64-linux-gnu/sys/select.h \
+ /usr/include/x86_64-linux-gnu/bits/select.h \
+ /usr/include/x86_64-linux-gnu/bits/types/sigset_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__sigset_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_timeval.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_timespec.h \
+ /usr/include/x86_64-linux-gnu/bits/pthreadtypes.h \
+ /usr/include/x86_64-linux-gnu/bits/thread-shared-types.h \
+ /usr/include/x86_64-linux-gnu/bits/pthreadtypes-arch.h \
+ /usr/include/x86_64-linux-gnu/bits/atomic_wide_counter.h \
+ /usr/include/x86_64-linux-gnu/bits/struct_mutex.h \
+ /usr/include/x86_64-linux-gnu/bits/struct_rwlock.h /usr/include/X11/X.h \
+ /usr/include/X11/Xfuncproto.h /usr/include/X11/Xosdefs.h \
+ /usr/include/X11/Xutil.h /usr/include/X11/keysym.h \
+ /usr/include/X11/keysymdef.h /usr/include/GL/gl.h \
+ /usr/include/GL/glext.h /usr/include/KHR/khrplatform.h \
+ /usr/lib/gcc/x86_64-linux-gnu/11/include/stdint.h /usr/include/stdint.h \
+ /usr/include/x86_64-linux-gnu/bits/libc-header-start.h \
+ /usr/include/x86_64-linux-gnu/bits/wchar.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-uintn.h \
+ /usr/include/GL/glxext.h /usr/include/inttypes.h
